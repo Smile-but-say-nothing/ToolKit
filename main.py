8114cbef5bc9f4d55bb4948325bd3f8fb51f4d8d
SuperWG <1277557316@qq.com>
2022-03-22 21:42:25 +0800

    更新了代码与文档，提交了README.md

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QApplication, QWidget
 import os
 import pandas as pd
 
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(QMainWindow, self).__init__(parent)
@@ -13,7 +14,9 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
     def get_file_directory(self):
-        fileDescription = QFileDialog.getOpenFileName(self, "选取文件", os.getcwd(), "All Files(*);;Text Files(*.txt);;Excel Files(*.xlsx)")
+        # 创建getOpenFileName对象
+        fileDescription = QFileDialog.getOpenFileName(self, "选取文件", os.getcwd(),
+                                                      "All Files(*);;Text Files(*.txt);;Excel Files(*.xlsx)")
         filePath = str(fileDescription[0])
         # 显示文件路径
         self.ui.lineEdit.setText(filePath)
@@ -38,29 +41,33 @@ class MainWindow(QMainWindow):
                     f.write(response.read())  # 将内容写入图片
                 return filename
 
+        # 获取文件路径
         filePath = self.ui.lineEdit.text()
+        # 保存到哪个文件夹
         dstPath = self.ui.lineEdit_5.text() + '/'
+        # 哪一列是链接列
         urlCol = self.ui.lineEdit_4.text()
-        df = pd.read_excel(filePath, )
+        df = pd.read_excel(filePath, dtype=str)
         dfUrl = df[urlCol]
+        # 输出图片的格式
         pattern = self.ui.lineEdit_3.text().split(' ')
         patternList = []
+        # 将列封装到list里
         for item in pattern:
             patternList.append(df[item])
-        for i in range(df.shape[0]-1):
+        for i in range(df.shape[0] - 1):
             try:
-                imgurl = dfUrl[i]
-                imgname = [str(patternList[order][i]) for order in range(len(pattern))]
-                imgname = str('-'.join(imgname))
-                download_img(imgurl, imgname, dstPath)
-                print('第' + str(i) + '张图下载完成！')
+                imgUrl = dfUrl[i]
+                imgName = [str(patternList[order][i]) for order in range(len(pattern))]
+                imgName = str('-'.join(imgName))
+                download_img(imgUrl, imgName, dstPath)
+                print(f'imgName:{imgName} imgUrl:{imgUrl}')
             except BaseException as e:
-                print(e)
+                print(f'Error:{e}')
                 continue
 
 
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = MainWindow()
     MainWindow.show()
